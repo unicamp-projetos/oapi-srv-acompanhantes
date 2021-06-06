da4d1d8086a494d0515176a3c2245ea5b733471a
Natan Rodrigues de Oliveira <natanrodriguesdeoliveira@gmail.com>
2021-06-06 10:06:28 -0300

    repo created

---
+++ src/main/java/br/unicamp/mc851/evisita/oapisrvacompanhantes/OapiSrvAcompanhantesApplication.java
@@ -0,0 +1,13 @@
+package br.unicamp.mc851.evisita.oapisrvacompanhantes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OapiSrvAcompanhantesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OapiSrvAcompanhantesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/unicamp/mc851/evisita/oapisrvacompanhantes/OapiSrvAcompanhantesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.unicamp.mc851.evisita.oapisrvacompanhantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OapiSrvAcompanhantesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
